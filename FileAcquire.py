6033b155a29c87883806fdfc12eea171b6c7728c
lindahu123 <lhu@olin.edu>
2020-10-30 13:26:16 -0700

    Add code to convert pdf to csv

--- FileAcquire.py
+++ FileAcquire.py
@@ -1,21 +1,42 @@
 from pathlib import Path
 import requests
+import tabula
+import pandas as pd
 
 
-def save_pdfs_locally(sites, filenames):
+def save_pdfs_locally(sites, filenames, folder):
     """
     acquires data from a list of sites and saves the files locally
 
     Args:
         sites: a list of links for the PDFs
-        filenames: a list of filenames for the corresponding PDFs
+        filenames: base filename for the corresponding PDFs
 
     Returns:
         locally saved PDFs in the root directory
     """
     for i, site in enumerate(sites):
-        file_name = Path(filenames[i])
+        file_name = Path('{0}/{1}.pdf'.format(folder, filenames[i]))
         url = site
         response = requests.get(url)
         file_name.write_bytes(response.content)
     return print("files saved locally")
+
+
+def pdf_to_dataframe(pdf_names, folder_origin, folder_final, area=None):
+    """
+    extracts dataframes from pdfs
+
+    Args:
+        file: 
+
+    Returns:
+        locally saved csv files
+    """
+    for files in pdf_names:
+        pdf_file = Path('{0}/{1}.pdf'.format(folder_origin, files))
+        table = tabula.read_pdf(pdf_file, area=area, pages=1)
+        table = table[0]
+        csv_name = Path('{0}/{1}.csv'.format(folder_final, files))
+        table.to_csv(csv_name, index=False, header=False)
+    pass
---
+++ ProcessData.py
@@ -0,0 +1,15 @@
+def extract_useful_data():
+    """
+    Extracts Data
+
+    Args:
+
+
+    Returns:
+        CSV eith
+    """
+    pass
+
+
+def plot_data():
+    pass
